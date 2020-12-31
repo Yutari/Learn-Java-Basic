27b3d5ed23152928ff1c318d417f3a333e48d580
Yutari <yutari01@hanyang.ac.kr>
2020-12-31 05:32:52 +0000

    Ch3

---
+++ Ch3/OperatorEx2.java
@@ -0,0 +1,13 @@
+class OperatorEx2 {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 0;
+        j = i++;
+        System.out.println("j=i++; after, i="+i+",j="+j);
+
+        i = 5;
+        j = 0;
+        j = ++i;
+        System.out.println("j=++i; after, i="+i+",j="+j);
+    }    
+}
