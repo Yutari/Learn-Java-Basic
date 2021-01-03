9c6d7058d530be156f2e0d5dc094d021e7138e5d
Yutari <yutari01@hanyang.ac.kr>
2021-01-03 07:42:01 +0000

    Ch3

---
+++ Ch3/OperatorEx10.java
@@ -0,0 +1,9 @@
+class OperatorEx10 {
+    public static void main(String[] args) {
+        int a = 1000000;
+        int b = 1000000;
+
+        long c = a * b;
+        System.out.println(c);
+    }
+}
---
+++ Ch3/OperatorEx19.java
@@ -0,0 +1,8 @@
+class OperatorEx19 {
+    public static void main(String[] args) {
+        int share = 10/8;
+        int remain = 10%8;
+        System.out.println("share : "+share);
+        System.out.println("remain : "+remain);
+    }    
+}
