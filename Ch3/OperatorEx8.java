ed859f1c5b115eec85c169d6122ccf307a89be25
Yutari <yutari01@hanyang.ac.kr>
2021-01-03 06:59:45 +0000

    Ch3

---
+++ Ch3/OperatorEx8.java
@@ -0,0 +1,10 @@
+class OperatorEx8 {
+    public static void main(String[] args){
+        byte a = 10;
+        byte b = 20;
+        byte c = a+b; //int
+        //byte c = (byte)(a+b); no error
+
+        System.out.println(c);
+    }
+}
