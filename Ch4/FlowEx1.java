62e80eba4ccf7e35d28d9760a727560ec6ef7679
Yutari <yutari01@hanyang.ac.kr>
2021-01-03 07:25:10 +0000

    Ch4

---
+++ Ch4/FlowEx1.java
@@ -0,0 +1,8 @@
+class FlowEx1 {
+    public static void main(String[] args) {
+        int visitCount = 0;
+        if (visitCount < 1) {
+            System.out.println("Welcome");
+        }
+    }
+}
